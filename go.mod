8c96202be85597f0ad9686a12ff7b72cbbe0dc77
0xDanW-ml <dwong@moneylion.com>
2025-01-23 11:05:13 +0800

    Init repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/0xDanW/go-for-exp-programmers/stringutils
+
+go 1.23.5
---
+++ stringutils.go
@@ -0,0 +1,11 @@
+package stringutils
+
+import "strings"
+
+func Lower(s string) string {
+	return s.ToLower(s)
+}
+
+func Upper(s string) string {
+	return s.ToUpper(s)
+}
